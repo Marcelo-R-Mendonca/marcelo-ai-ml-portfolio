11fd2507249ba8a6eeb50b53a5605458f3c06998
Marcelo Mendonca <marcelomendonca84@gmail.com>
2020-06-28 18:28:55 -0500

    more changes

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   // publicPath      : process.env.NODE_ENV === 'production' ? '/' : '/',
-//   css             : { loaderOptions: { css: { url: false } } },
-//   configureWebpack: (config) => {
-//     config.module.rules = [
-//       ...config.module.rules,
-//       {
-//         test   : require('path').resolve(__dirname, 'node_modules/leader-line/'),
-//         loader : 'skeleton-loader',
-//         options: { procedure: (content) => `${content} export default LeaderLine` },
-//       },
-//     ]
-//   },
-// }
+module.exports = {
+  // publicPath      : process.env.NODE_ENV === 'production' ? '/' : '/',
+  css             : { loaderOptions: { css: { url: false } } },
+  configureWebpack: (config) => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test   : require('path').resolve(__dirname, 'node_modules/leader-line/'),
+        loader : 'skeleton-loader',
+        options: { procedure: (content) => `${content} export default LeaderLine` },
+      },
+    ]
+  },
+}
 // vue.config.js
-module.exports = { publicPath: '/marcelo-ai-ml-portfolio/' }
+// module.exports = { publicPath: '/marcelo-ai-ml-portfolio/' }
